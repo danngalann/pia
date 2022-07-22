a49c2c9213945de86acff02dd126494eec52ee0f
danngalann <47415745+danngalann@users.noreply.github.com>
2022-07-22 19:30:07 +0200

    Add user model

---
+++ server/models/user.model.js
@@ -0,0 +1,49 @@
+const { Schema } = require('mongoose');
+const { isEmail } = require('validator');
+
+const SALT_WORK_FACTOR = 10;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: [isEmail, 'invalid email'],
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', next => {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.validatePassword = (candidatePassword, cb) => {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
