8c4c5106382396aab608475dc8a1c097a3f93ef4
danngalann <47415745+danngalann@users.noreply.github.com>
2022-07-23 12:01:16 +0200

    Actually, a subdocument is not necessary in this case

--- server/models/incident.model.js
+++ server/models/incident.model.js
@@ -1,24 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-const traceSchema = new Schema({
-  file: {
-    type: String,
-    required: true,
-  },
-  line: {
-    type: Number,
-    required: true,
-  },
-  function: {
-    type: String,
-    required: false,
-  },
-  args: {
-    type: Array,
-    required: false,
-  },
-});
-
 const incidentSchema = new Schema(
   {
     project: {
@@ -42,7 +23,7 @@ const incidentSchema = new Schema(
       index: true,
     },
     trace: {
-      type: [traceSchema],
+      type: Array,
       required: true,
     },
   },
