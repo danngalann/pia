23e9fcfc1675f29f91feea1af2cf954b51887362
danngalann <47415745+danngalann@users.noreply.github.com>
2022-07-31 15:06:39 +0200

    Add error boundary to incidents suspense

--- client/src/pages/Incidents.js
+++ client/src/pages/Incidents.js
@@ -3,13 +3,16 @@ import React, { Suspense } from 'react';
 import IncidentList from '../components/incidents/IncidentList';
 import Page from '../components/page/Page';
 import CenteredLoader from '../components/common/CenteredLoader';
+import { ErrorBoundary } from '../components/common/ErrorBoundary';
 
 export default function Incidents() {
   return (
     <Page>
-      <Suspense fallback={<CenteredLoader />}>
-        <IncidentList />
-      </Suspense>
+      <ErrorBoundary fallback={<h2>Could not fetch incidents.</h2>}>
+        <Suspense fallback={<CenteredLoader />}>
+          <IncidentList />
+        </Suspense>
+      </ErrorBoundary>
     </Page>
   );
 }
