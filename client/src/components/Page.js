950263db5a7446cd0f6501fa4fee055e5d619627
danngalann <47415745+danngalann@users.noreply.github.com>
2022-07-31 12:06:21 +0200

    Add AppShell for pages

---
+++ client/src/components/Page.js
@@ -0,0 +1,6 @@
+import { AppShell } from '@mantine/core';
+import React from 'react';
+
+export default function Page({ children }) {
+  return <AppShell>{children}</AppShell>;
+}
--- client/src/pages/Incidents.js
+++ client/src/pages/Incidents.js
@@ -2,11 +2,14 @@ import React, { Suspense } from 'react';
 import { Loader } from '@mantine/core';
 
 import IncidentList from '../components/IncidentList';
+import Page from '../components/Page';
 
 export default function Incidents() {
   return (
-    <Suspense fallback={<Loader />}>
-      <IncidentList />
-    </Suspense>
+    <Page>
+      <Suspense fallback={<Loader />}>
+        <IncidentList />
+      </Suspense>
+    </Page>
   );
 }
