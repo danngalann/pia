e8fdfd18c55eebd43649736ae2e0f5e9fbe8caff
danngalann <47415745+danngalann@users.noreply.github.com>
2022-08-03 18:17:52 +0200

    Completed test of refresh token

--- server/auth/authenticationMiddleware.js
+++ server/auth/authenticationMiddleware.js
@@ -2,9 +2,14 @@ const jwt = require('jsonwebtoken');
 
 function authenticationMiddleware(req, res, next) {
   const token = req.cookies.accessToken;
+  const refreshToken = req.cookies.refreshToken;
 
   if (!token) {
-    return res.status(401).json({ error: 'No token provided' });
+    return refreshToken
+      ? res.status(403).json({
+          error: 'No access token was found, but refresh token provided. Do you need to refresh the access token?',
+        })
+      : res.status(401).json({ error: 'No token provided' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
