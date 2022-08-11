2b0f86f2383f5ee5dbcbeae0aae59b75343c30ea
danngalann <47415745+danngalann@users.noreply.github.com>
2022-08-11 10:30:34 +0200

    Added occurrence list to incident detail

--- client/src/components/incidents/IncidentDetail.js
+++ client/src/components/incidents/IncidentDetail.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Code, Modal, Select } from '@mantine/core';
+import { Code, Grid, List, Modal, ScrollArea, Select, Text } from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
 import { IconX, IconCheck } from '@tabler/icons';
 
@@ -24,7 +24,7 @@ export default function IncidentDetail({ incidentId, setIncidentId }) {
 }
 
 function IncidentModalContent({ incident }) {
-  const dateFormatted = new Date(incident.createdAt).toLocaleString();
+  const dateFormatted = new Date(incident.ocurred_at[0]).toLocaleString();
   const [status, setStatus] = useState(incident.status);
 
   const updateStatus = newStatus => {
@@ -54,24 +54,48 @@ function IncidentModalContent({ incident }) {
   };
 
   return (
-    <div>
-      <h1>{incident.project}</h1>
-      <p>
-        The error <Code>{incident.message}</Code> was first received at {dateFormatted}.
-      </p>
-      <Select
-        label="Status"
-        data={[
-          { value: 'open', label: 'Open' },
-          { value: 'investigating', label: 'Investigating' },
-          { value: 'identified', label: 'Identified' },
-          { value: 'closed', label: 'Closed' },
-        ]}
-        value={status}
-        onChange={updateStatus}
-      />
-      <p>The trace was:</p>
-      <Code block>{JSON.stringify(incident.trace, null, 2)}</Code>
-    </div>
+    <Grid>
+      <Grid.Col span={6}>
+        <h1>{incident.project}</h1>
+      </Grid.Col>
+      <Grid.Col span={6}>
+        <Select
+          label="Status"
+          data={[
+            { value: 'open', label: 'Open' },
+            { value: 'investigating', label: 'Investigating' },
+            { value: 'identified', label: 'Identified' },
+            { value: 'closed', label: 'Closed' },
+          ]}
+          value={status}
+          onChange={updateStatus}
+        />
+      </Grid.Col>
+      <Grid.Col span={6}>
+        <Text>The error was first received at {dateFormatted}.</Text>
+        <Text>
+          The message was: <Code>{incident.message}</Code>
+        </Text>
+      </Grid.Col>
+      <Grid.Col span={6}>
+        <Text>Occurrences:</Text>
+        <ScrollArea
+          style={{ height: 150, width: 300 }}
+          type="auto"
+          offsetScrollbars
+        >
+          <List type="ordered">
+            {incident.ocurred_at.map(date => {
+              const dateFormatted = new Date(date).toLocaleString();
+              return <List.Item key={date}>{dateFormatted}</List.Item>;
+            })}
+          </List>
+        </ScrollArea>
+      </Grid.Col>
+      <Grid.Col span={12}>
+        <p>The trace was:</p>
+        <Code block>{JSON.stringify(incident.trace, null, 2)}</Code>
+      </Grid.Col>
+    </Grid>
   );
 }
